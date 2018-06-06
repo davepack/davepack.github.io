a408433c702ec1ce2b2f9bd43a3de419165898e8
David Pack <davepack@icloud.com>
2018-06-06 10:05:44 -0700

    Updates

--- path----b78c6c2f3190c5ffa7b0.js
+++ path----b78c6c2f3190c5ffa7b0.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{226:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Portfolio | Dave Pack"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{227:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Portfolio | Dave Pack"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----b78c6c2f3190c5ffa7b0.js.map
